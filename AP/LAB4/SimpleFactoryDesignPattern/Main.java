3cc4fbf6ba706ad3725170a1e38f2c7bc5ae6fa6
047pegasus <tanishqoct11@gmail.com>
2024-02-24 10:58:16 +0530

    add: .gitignore

--- AP/LAB4/SimpleFactoryDesignPattern/Main.java
+++ AP/LAB4/SimpleFactoryDesignPattern/Main.java
@@ -1,5 +1,11 @@
 package SimpleFactoryDesignPattern;
 // Write a program to simulate Simple Factory Design Pattern for a tea store. The tea store has 3 types of tea: Green Tea, Black Tea, and Lemon Tea. The program should have a TeaStore class that has a method to create a tea object based on the type of tea. The program should have a Tea interface and 3 classes that implement the Tea interface: GreenTea, BlackTea, and LemonTea. The program should have a TeaFactory class that has a method to create a tea object based on the type of tea. The TeaStore class should use the TeaFactory class to create a tea object. The program should have a main method that demonstrates the use of the Simple Factory Design Pattern. The program should display the type of tea and the price of the tea. The program should solve the problem to open a class while adding a new type of tea to the store and use decoupling to solve the problem of new tea type addition to the store.
+// import SimpleFactoryDesignPattern.Tea;
+// import SimpleFactoryDesignPattern.TeaStore;
+// import SimpleFactoryDesignPattern.GreenTea;
+// import SimpleFactoryDesignPattern.BlackTea;
+// import SimpleFactoryDesignPattern.LemonTea;
+// import SimpleFactoryDesignPattern.TeaFactory;
 
 public class Main {
     public static void main(String[] args) {
