f27d4f6f91eab0aac16c82c35f92d5e9cfe24199
047pegasus <tanishqoct11@gmail.com>
2024-03-16 10:43:30 +0530

    mod: AP_LAB4: Decorator-DP Main.java

--- AP/LAB4/DecoratorDesignPattern/Main.java
+++ AP/LAB4/DecoratorDesignPattern/Main.java
@@ -1,9 +1,9 @@
 package AP.LAB4.DecoratorDesignPattern;
-
 public class Main {
     public static void main(String[] args) {
         Offering offering = new Coffee();
         offering = new Rum(offering); //coffee is wrapped by rum decorator
+        // offering = new Rum(offering); //coffee with double rum
         System.out.println(offering.getName());
         System.out.println(offering.getPrice());
     }
